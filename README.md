752d0d0068d6ba3bad09379267c28e87f0c8a7a4
Tomasz Janczuk <tomasz@janczuk.org>
2015-05-06 11:23:46 -0700

    move to streaming api

---
+++ README.md
@@ -0,0 +1,20 @@
+Run code by Twitter with webtask.io
+====
+
+This project demonstrates the use of [webtask.io](https://webtask.io) created by [Auth0](https://auth0.com) to run Node.js code submitted to the [@webtaskio](https://twitter.com/webtaskio) twitter alias by way of a tweet. 
+
+## How to use
+
+Using your favorite Twitter client, send a tweet formatted as follows:
+
+```
+run @webtaskio: cb => cb(null, 'Hello, world!')
+```
+
+Moments later you should receive a response via Twitter containing the results of the execution. 
+
+You can submit any webtask Node.js code for execution (well it needs to fit in a Tweet). Read more at https://webtask.io.
+
+## How it works
+
+The Twitter client in this project attaches to the user stream of [@webtaskio](https://twitter.com/webtaskio). It filters the tweets that match the syntax above. It extracts the Node.js code. It then executes the code using [webtask.io](https://webtask.io). Lastly, it sends the result of execution to you by way of a Twitter reply. 
--- server.js
+++ server.js
@@ -1,18 +1,15 @@
 require('dotenv').load();
-var async = require('async')
-    , fs = require('fs')
-    , path = require('path')
-    , assert = require('assert')
+var assert = require('assert')
     , request = require('request')
     , Entities = require('html-entities').AllHtmlEntities
-    , entities = new Entities();
+    , entities = new Entities()
+    , Twitter = require('twitter');
 
 [
 'CONSUMER_KEY',
 'CONSUMER_SECRET',
 'ACCESS_TOKEN',
 'ACCESS_TOKEN_SECRET',
-'DELAY',
 'WEBTASK_URL',
 'WEBTASK_TOKEN'
 ].forEach(function (v) { 
@@ -20,119 +17,53 @@ var async = require('async')
 });
 
 var logger = require('bunyan').createLogger({ name: 'webtask-twitter' });
-var Twitter = require('twitter-node-client').Twitter;
+var twitter = new Twitter({
+    "consumer_key": process.env.CONSUMER_KEY,
+    "consumer_secret": process.env.CONSUMER_SECRET,
+    "access_token_key": process.env.ACCESS_TOKEN,
+    "access_token_secret": process.env.ACCESS_TOKEN_SECRET
+});
 
-var config = {
-    "consumerKey": process.env.CONSUMER_KEY,
-    "consumerSecret": process.env.CONSUMER_SECRET,
-    "accessToken": process.env.ACCESS_TOKEN,
-    "accessTokenSecret": process.env.ACCESS_TOKEN_SECRET
-};
+twitter.stream('user', {}, function (stream) {
+    logger.info('attached to twitter stream');
+    stream.on('data', process_tweet);
+    stream.on('error', function(error) {
+        logger.error(error, 'error, exiting process in 500ms');
+        setTimeout(function () {
+            process.exit(1);
+        }, 500);
+    });
+});
 
-var twitter = new Twitter(config);
-var max_id, since_id, new_since_id;
-var state_file = path.join(__dirname, 'state.json');
-
-try {
-    since_id = require(state_file).since_id;
-    logger.warn({ since_id: since_id }, 'restored bookmark');
-}
-catch (e) {
-    logger.warn('no bookmark restored');
-}
-
-async.forever(
-    function (next) {
-        async.doWhilst(
-            function (callback) {
-
-                twitter.getSearch({
-                    q: '@webtaskio',
-                    count: 5,
-                    since_id: since_id,
-                    max_id: max_id || since_id
-                }, on_error, on_data);
-
-                function on_data(data) {
-                    data = JSON.parse(data);
-                    logger.info({ 
-                        count: data.statuses.length, 
-                        max_id: max_id, 
-                        since_id: since_id, 
-                        new_since_id: new_since_id 
-                    }, 'result');
-                    if (data.statuses.length > 0) {
-                        max_id = data.statuses[data.statuses.length - 1].id - 1;
-                        if (!new_since_id)
-                            new_since_id = data.statuses[0].id;
-                        async.eachLimit(data.statuses, 5, process_tweet, callback);
-                    }
-                    else {
-                        if (new_since_id) {
-                            since_id = new_since_id;
-                            logger.warn({ since_id: since_id }, 'storing bookmark');
-                            fs.writeFileSync(state_file, JSON.stringify({ since_id: since_id }), 'utf8');
-                        }
-                        max_id = new_since_id = undefined;
-                        callback();
-                    }
-                }
-
-                function on_error(error, res, body) {
-                    logger.error(error, 'error');
-                    callback();
-                }
-            },
-            function () { 
-                logger.info({ 
-                    continue_paging: max_id !== undefined && since_id !== undefined, 
-                    max_id: max_id, 
-                    since_id: since_id, 
-                    new_since_id: new_since_id 
-                }, 'paging test');
-                return max_id !== undefined && since_id !== undefined; 
-            },
-            function (error) {
-                error ? next(error) : setTimeout(next, +process.env.DELAY);
-            }
-        )
-    },
-    function (error) {
-        logger.error(error, 'error');
-    }
-);
-
-var run_code = /^\s*run\s+\@webtaskio\:(.*)/i;
+var run_code = /^\s*run\s+\@webtaskio(?:\:|\s+)\s*(.*)$/i;
 var suffix = ' /via https://webtask.io';
-function process_tweet(tweet, callback) {
+function process_tweet(tweet) {
+    if (typeof tweet !== 'object')
+        return;
+    if (!tweet.text || !tweet.user.screen_name || !tweet.id)
+        return;
+    tweet.text = entities.decode(tweet.text);
     logger.info({ 
         id: tweet.id, 
         user: tweet.user.screen_name, 
         text: tweet.text 
-    }, 'considering tweet');
+    }, 'incoming');
     if (tweet.retweeted_status) {
-        logger.info({ id: tweet.id }, 'ignoring retweet');
-        return callback(); // ignore retweets
+        logger.info({ id: tweet.id }, 'ignore: retweet');
+        return;
     }
-    var match = entities.decode(tweet.text).match(run_code);
+    var match = tweet.text.match(run_code);
     if (!match) {
-        logger.info({ id: tweet.id }, 'ignoring tweet that does not match pattern');
-        return callback(); // ignore tweets that are not request to run code
+        logger.info({ id: tweet.id }, 'ignore: no match');
+        return;
     }
-    var i = match[1].lastIndexOf('}');
-    if (i < 0) 
-        i = match[1].lastIndexOf(';');
-    if (i < 0) {
-        logger.info({ id: tweet.id }, 'ignoring tweet that does not have ; or }');
-        return callback(); // does not contain a function
-    }
-    var code = match[1].substring(0, i + 1).trim();
-    logger.info({ 
+    var code = match[1].trim();
+    logger.warn({ 
         id: tweet.id, 
         user: tweet.user.screen_name, 
         code: code 
-    }, 'tweet has code');
-    if (code.lastIndexOf('return') !== 0)
+    }, 'match');
+    if (code.indexOf('return') !== 0)
         code = '"use latest";\nreturn ' + code;
     else
         code = '"use latest";\n' + code;
@@ -159,7 +90,7 @@ function process_tweet(tweet, callback) {
                 container: container, 
                 body: body 
             }, 'webtask response');
-            msg = '@webtaskio result: ' + body;
+            msg = 'result from @webtaskio: ' + body;
         }
         else {
             logger.warn({ 
@@ -176,18 +107,16 @@ function process_tweet(tweet, callback) {
             msg = msg.substring(0, 132 - suffix.length) + '...';
         msg += suffix;
 
-        logger.warn({ result: msg }, 'twitter response');
-
-        twitter.postTweet({
+        twitter.post('statuses/update', {
             status: msg,
-            in_reply_to_status_id: tweet.id,
+            in_reply_to_status_id: tweet.id.toString()
         }, function (error) {
             if (error)
-                logger.error(error, 'error posting to twitter');
-            return callback();
-        }, function (data) {
-            logger.warn({ result: msg }, 'twitter response sent');
-            return callback();
+                return logger.error({ 
+                    msg: msg, 
+                    error: error.message || error.toString() 
+                }, 'error sending response');
+            logger.warn({ result: msg }, 'sent response');
         });
     });
 }
